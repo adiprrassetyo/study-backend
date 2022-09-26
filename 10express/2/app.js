d132873a551cfc6b7adf9680746daa0098393d9f
adeeep14 <disconetct@gmail.com>
2022-09-26 13:43:52 +0700

    y

--- 10express/2/app.js
+++ 10express/2/app.js
@@ -1,38 +1,64 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const expressLayouts = require("express-ejs-layouts");
+
+//gunakan ejs
+app.set("view engine", "ejs");
+app.use(expressLayouts);
 
 app.get("/", (req, res) => {
-  // res.send("Hello World!");
-  // res.json({
-  //   nama: "Sandhika",
-  //   email: "sandhika@gmail,com",
-  //   noHP: "081294717383",
-  // });
-  res.sendFile("./index.html", { root: __dirname });
+  const mahasiswa = [
+    {
+      nama: "Adi Prasetyo",
+      email: "adi@gmail.com",
+    },
+    {
+      nama: "cipto",
+      email: "cipto@gmail.com",
+    },
+    {
+      nama: "dadang",
+      email: "dadang@gmail.com",
+    },
+  ];
+
+  // res.sendFile("./index.html", { root: __dirname });
+  res.render("index", {
+    nama: "Adi Prasetyo",
+    layout: "layouts/main-layouts",
+    title: "Halaman Home",
+    mahasiswa,
+  });
 });
 
 app.get("/about", (req, res) => {
-  // res.send("Ini halaman about");
-  res.sendFile("./about.html", { root: __dirname });
+  // res.sendFile("./about.html", { root: __dirname });
+  res.render("about", {
+    layout: "layouts/main-layouts",
+    title: "Halaman HAbout",
+  });
 });
 
 app.get("/contact", (req, res) => {
-  // res.send("Ini halaman contact");
-  res.sendFile("./contact.html", { root: __dirname });
+  // res.sendFile("./contact.html", { root: __dirname });
+  res.render("contact", {
+    layout: "layouts/main-layouts",
+    title: "Halaman Contact",
+  });
 });
 
-// app.get("/product/:id/category/:id", (req, res) => {
-//   res.send(
-//     `Product ID :  ${req.params.id} <br> Category ID : ${req.params.id}`
-//   );
-// });
 app.get("/product/:id/", (req, res) => {
   res.send(
-    `Product ID :  ${req.params.id} <br> Category Name : ${req.query.category}`
-    // http://localhost:3000/product/50?category=shoes
+    `Product ID :  ${req.params.id} <br> Category ID : ${req.params.id}`
   );
 });
+// app.get("/product/:id/category/:query", (req, res) => {
+//   res.send(
+//     `Product ID :  ${req.params.id} <br> Category Name : ${req.query.category}`
+//     // http://localhost:3000/product/50?category=shoes
+//   );
+// });
 
 app.use("/", (req, res) => {
   res.status(404);
@@ -42,52 +68,3 @@ app.use("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
 });
-
-// const http = require("node:http");
-// const fs = require("node:fs");
-
-// const port = 3000;
-
-// const renderHTML = (path, res) => {
-//   fs.readFile(path, (err, data) => {
-//     if (err) {
-//       res.writeHead(404);
-//       res.write("Error : file not found");
-//     } else {
-//       res.write(data);
-//     }
-//     res.end();
-//   });
-// };
-
-// http
-//   .createServer((req, res) => {
-//     res.writeHead(200, {
-//       "Content-Type": "text/html",
-//     });
-
-//     const url = req.url;
-//     switch (url) {
-//       case "/about":
-//         renderHTML("./about.html", res);
-//         break;
-//       case "/contact":
-//         renderHTML("./contact.html", res);
-//         break;
-//       default:
-//         renderHTML("./index.html", res);
-//         break;
-//     }
-
-//     // const url = req.url;
-//     // if (url === "/about") {
-//     //   renderHTML("./about.html", res);
-//     // } else if (url === "/contact") {
-//     //   renderHTML("./contact.html", res);
-//     // } else {
-//     //   renderHTML("./index.html", res);
-//     // }
-//   })
-//   .listen(port, () => {
-//     console.log(`Server is running on server ${port}...`);
-//   });
